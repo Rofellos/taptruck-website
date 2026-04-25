cc7a8555b7938bd800383d103e0e6c9ceca08588
Adam Kuzminski <adomas.kuzminski@gmail.com>
2026-04-25 14:45:04 -0700

    updated call click

--- src/lib/components/CallBar.svelte
+++ src/lib/components/CallBar.svelte
@@ -1,5 +1,21 @@
+<script lang="ts">
+	import { trackEvent } from '$lib/utils/analytics';
+</script>
+
 <a
 	href="tel:14373500049"
+    on:click={(e) => {
+	e.preventDefault();
+
+	trackEvent('call_click', {
+		event_category: 'engagement',
+		event_label: 'floating_call_button'
+	});
+	setTimeout(() => {
+		window.location.href = 'tel:14373500049';
+	}, 150);
+    }}
+    
 	class="
 		fixed bottom-4 right-4
 		inline-flex items-center gap-2 rounded-full
@@ -10,6 +26,8 @@
 		sm:left-auto sm:right-6 sm:translate-x-0 sm:gap-3 sm:px-5
 	"
 	aria-label="Call Tap Truck VI to book now"
+
+    
 >
 	<!-- Icon -->
 	<span class="flex h-10 w-10 items-center justify-center rounded-full bg-white/10">
